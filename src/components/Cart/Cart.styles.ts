03e2686c14050b9733eb23b55ee52cb51fd6331a
Michael Hutchinson <michael-hutchinson@hotmail.co.uk>
2022-02-16 20:31:50 +0000

    create cart component

---
+++ src/components/Cart/Cart.styles.ts
@@ -0,0 +1,8 @@
+/* eslint-disable import/prefer-default-export */
+import styled from 'styled-components';
+
+export const Wrapper = styled.aside`
+  font-family: Arial, Helvetica, sans-serif;
+  width: 500px;
+  padding: 20px;
+`;
---
+++ src/components/Cart/Cart.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Wrapper } from './Cart.styles';
+
+const Cart: React.FunctionComponent = () => (
+  <Wrapper>
+    <p>Test</p>
+  </Wrapper>
+);
+
+export default Cart;
