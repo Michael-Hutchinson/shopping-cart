ce66c71058110d0b096ffb1743ab1040c4ced19c
Michael Hutchinson <michael-hutchinson@hotmail.co.uk>
2022-02-16 19:24:53 +0000

    create a styled button for shopping cart

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import LinearProgress from '@material-ui/core/LinearProgress';
 import Grid from '@material-ui/core/Grid';
 import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 import Badge from '@material-ui/core/Badge';
-import { Wrapper } from './App.styles';
+import { Wrapper, StyledButton } from './App.styles';
 import { CartItemType } from './models/cart.model';
 import Item from './components/Item/Item';
 
@@ -13,6 +13,8 @@ const getProducts = async (): Promise<CartItemType[]> =>
   (await fetch('https://fakestoreapi.com/products')).json();
 
 const App: React.FunctionComponent = () => {
+  const [cartOpen, setCartOpen] = useState(false);
+  const [cartItems, setCartItems] = useState([] as CartItemType[]);
   const { data, isLoading, error } = useQuery<CartItemType[]>(
     'products',
     getProducts
@@ -25,6 +27,10 @@ const App: React.FunctionComponent = () => {
   if (error) return <div>Something went wrong ...</div>;
   return (
     <Wrapper>
+      <Drawer anchor="right" open={cartOpen} onClose={() => setCartOpen(false)}>
+        Hello
+      </Drawer>
+      <StyledButton onClick={() => setCartOpen(true)}>Hello</StyledButton>
       <Grid container spacing={3}>
         {data?.map((item) => (
           <Grid item key={item.id} xs={12} sm={4}>
