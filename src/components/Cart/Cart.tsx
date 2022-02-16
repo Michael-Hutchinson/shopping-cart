1ae472f4c3901a32e8910c21b29dbb2202115a98
Michael Hutchinson <michael-hutchinson@hotmail.co.uk>
2022-02-16 20:51:14 +0000

    add cart interface

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import CartItem from '../CartItem/CartItem';
+import { CartItemType } from '../../models/cart.model';
 import { Wrapper } from './Cart.styles';
 
-const Cart: React.FunctionComponent = () => (
+interface ICartItemProps {
+  cartItems: CartItemType[];
+  addToCart: (clickedItem: CartItemType) => void;
+  removeFromCart: (id: number) => void;
+}
+
+const Cart: React.FunctionComponent<ICartItemProps> = ({
+  cartItems,
+  addToCart,
+  removeFromCart,
+}) => (
   <Wrapper>
     <p>Test</p>
   </Wrapper>
