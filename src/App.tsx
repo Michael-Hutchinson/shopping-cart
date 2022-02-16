881fd8cba49b5f91eccefc55bfcb5d5e62271d47
Michael Hutchinson <michael-hutchinson@hotmail.co.uk>
2022-02-16 17:04:59 +0000

    add cart interface into own file

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,7 @@ import Grid from '@material-ui/core/Grid';
 import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 import Badge from '@material-ui/core/Badge';
 import { Wrapper } from './App.styles';
-
-export interface CartItemType {
-  id: number;
-  category: string;
-  description: string;
-  image: string;
-  price: number;
-  title: string;
-  amount: number;
-}
+import { CartItemType } from './models/cart.model';
 
 const getProducts = async (): Promise<CartItemType[]> =>
   (await fetch('https://fakestoreapi.com/products')).json();
