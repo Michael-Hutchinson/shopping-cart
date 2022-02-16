a84d1b52b6f3cb553624f2d1dcefed025034e52f
Michael Hutchinson <michael-hutchinson@hotmail.co.uk>
2022-02-16 21:26:32 +0000

    build out cart items for adding to basket

--- src/components/CartItem/CartItem.tsx
+++ src/components/CartItem/CartItem.tsx
@@ -15,7 +15,31 @@ const CartItem: React.FunctionComponent<ICartItemProps> = ({
   removeFromCart,
 }) => (
   <Wrapper>
-    <p>Test</p>
+    <div>
+      <h3>{item.title}</h3>
+      <div className="information">
+        <p>Price: £{item.price}</p>
+        <p>Total: £{(item.amount * item.price).toFixed(2)}</p>
+      </div>
+      <div className="buttons">
+        <Button
+          size="small"
+          variant="contained"
+          onClick={() => removeFromCart(item.id)}
+        >
+          -
+        </Button>
+        <p>{item.amount}</p>
+        <Button
+          size="small"
+          variant="contained"
+          onClick={() => addToCart(item)}
+        >
+          +
+        </Button>
+      </div>
+    </div>
+    <img src={item.image} alt={item.title} />
   </Wrapper>
 );
 
