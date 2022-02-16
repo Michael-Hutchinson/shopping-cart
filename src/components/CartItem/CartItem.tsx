33b4e996f022e7528fb300e12ccea19d0487ea59
Michael Hutchinson <michael-hutchinson@hotmail.co.uk>
2022-02-16 20:37:57 +0000

    create cart item component

---
+++ src/components/CartItem/CartItem.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Wrapper } from './CartItem.styles';
+
+const CartItem: React.FunctionComponent = () => (
+  <Wrapper>
+    <p>Test</p>
+  </Wrapper>
+);
+
+export default CartItem;
